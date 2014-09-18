2dd490324041380e06eedd6569fd7e7ba8c74b9c
Samuel Lijin <sxlijin@gmail.com>
2014-09-17 20:27:55 -0500

    odata

---
+++ script.py
@@ -0,0 +1,13 @@
+import requests
+
+f = open('samplenew.csv','rU')
+contents = f.read().split('\n')
+url = 'http://services.odata.org/Northwind/Northwind.svc/Products/$count?$filter='
+newcontents = []
+
+for thing in contents:
+	newcontents.append(url+thing)
+
+for thing in newcontents:
+	print requests.get(thing)
+
